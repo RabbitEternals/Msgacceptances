28023f5001110d1e62bbdcba67a3b75c3a37b682
muhammed.gultekin <muhammed.gultekin@modanisa.com>
2021-11-26 14:25:08 +0300

    shopping test refactor finished

--- specs/shopping.spec
+++ specs/shopping.spec
@@ -1,41 +1,70 @@
-# Getting Started with Gauge
-
-This is a context step that runs before every scenario
+# Shoppin project test
 * Open localhost
 
-## Display number of items
-* Must display "Computer"
-* Must display "Cable"
-* Must display "Camera"
-* Must display "Mouse"
-* Must display "Shirt"
-* Must display "Couch"
-* Must display "Chair"
-* Must display "Table"
-* Must display "Monitor"
-* Must display "Car"
-* Click "ADD"
-* Click "ADD"
-* Click "ADD"
-* Click "ADD"
-* Click "ADD"
-* Click "ADD"
-* Click "ADD"
-* Click "ADD"
-* Click "ADD"
-* Click "ADD"
-* Click "Basket Page"
-* Must display "Computer"
-* Must display "Cable"
-* Must display "Camera"
-* Must display "Mouse"
-* Must display "Shirt"
-* Must display "Couch"
-* Must display "Chair"
-* Must display "Table"
-* Must display "Monitor"
-* Must display "Car"
+## Displays welcome header and basket link
+* Displays "welcome_header" "Welcome To ShoppingPage"
+* Displays "basket_link" "Basket Page"
+## Display initial 10 cards
+* Verify "card" from "0" to "9" exists
+* Displays "header" "Product " from "0" to "9" normal
+* Verify "photo" from "0" to "9" exists
+* Verify "name" from "0" to "9" exists
+* Verify "price" from "0" to "9" exists
+* Verify "button" from "0" to "9" exists
+## Add random three product to basket shirt650 camera1250 cable500
+* Click "button0" button
+* Click "button2" button
+* Click "button3" button
+* Verify "button0" button is not visible
+* Verify "button2" button is not visible
+* Verify "button3" button is not visible
+## Navigate to basket page and displays welcome header,shopping link, total amount, clear and purchase buttons
+* Click "basket_link" field
+* Displays "welcome_header_basket" "Welcome To Your Basket Card"
+* Displays "shopping_link" "Shopping Page"
+* Displays "clear_button" "Clear"
+* Displays "total_amount" "€2850"
+* Displays "purchase_button" "Purchase"
+## Navigate to basket page and displays products with all elements and compenents
+* Click "basket_link" field
+* Displays "name0" "Cable"
+* Displays "name1" "Chair"
+* Displays "name2" "Camera"
+* Displays "quantity0" "1"
+* Displays "quantity1" "1"
+* Displays "quantity2" "1"
+* Displays "price0" "500"
+* Displays "price1" "1100"
+* Displays "price2" "1250"
+* Verify "photo" from "0" to "2" exists
+* Displays "product" "Product " from "0" to "2" normal
+## Navigate to basket page and changing amount of product changes prices and total price correctly
+* Click "basket_link" field
+* Click "+0" button
+* Click "+1" button
+* Click "+1" button
+* Displays "quantity0" "2"
+* Displays "quantity1" "3"
+* Displays "price0" "1000"
+* Displays "price1" "3300"
+* Displays "total_amount" "€5550"
+* Click "-0" button
+* Click "-1" button
+* Displays "quantity0" "1"
+* Displays "quantity1" "2"
+* Displays "price0" "500"
+* Displays "price1" "2200"
+* Displays "total_amount" "€3950"
+## Navigate to basket page and delete product then does not show it
+* Click "basket_link" field
+* Click "delete2" button
+* Verify "card2" not exists
+* Displays "total_amount" "€2700"
+## Navigate to basket page and clkear products then does not show it
+* Click "basket_link" field
+* Click "clear_button" button
+* Verify "card0" not exists
+* Displays "total_amount" "€0"
 
-A tear down step for every scenario
 ___
 * Clear local
--- tests/step_implementation.js
+++ tests/step_implementation.js
@@ -46,88 +46,70 @@ step("Open localhost", async function () {
 step("Clear local", async function () {
     await evaluate(() => localStorage.clear());
 });
-//ECHO--FLASHCARD--LEDGER
 step("Enter <word> to <field_id> textbox", async function (word, field_id) {
     await write(word, into(textBox({id: field_id})));
 });
-//ECHO--COUNTER--FLASHCARD--LEDGER
 step("Click <button_id> button", async function (button_id) {
     await click(button({id: button_id}), {navigationTimeout: 3000});
 });
-//ECHO--FLASHCARD
 step("Verify <field_id> empty", async function (field_id) {
     assert.ok(await textBox({id: field_id}).value() == "");
 });
-//ECHO--COUNTER--FLASHCARD
 step("Must display <word> at <field_id>", async function (word, field_id) {
     var content = await evaluate($("#" + field_id), (element) => element.innerText);
     assert.ok(content == word)
 });
-//FLASHCARD
 step("Must display <word> at <field_class> class", async function (word, field_class) {
     var content = await evaluate($("." + field_class), (element) => element.textContent);
     var visible = await evaluate($("." + field_class), (element) => element.style.visibility);
     assert.ok(content == word && visible == "visible")
 });
-//FLASHCARD--LEDGER
 step("Verify <field_id> exists", async function (field_id) {
     assert.ok(await $("#" + field_id).exists());
 });
-//FLASHCARD
 step("Must not display <word> at <field_class> class", async function (word, field_class) {
     var content = await evaluate($("." + field_class), (element) => element.style.visibility);
     console.log(content)
     assert.ok(content == "hidden");
 });
-//LEDGER
 step("Must none display <word> at <field_class> class", async function (word, field_class) {
     var content = await evaluate($("." + field_class), (element) => element.style.display);
     console.log(content)
     assert.ok(content == "none");
 });
-//FLASHCARD
 step("Click <field_id> field", async function (field_id) {
     click($("#" + field_id));
 });
-//LEDGER
 step("Verify <field_id> from <start> to <stop> exists", async function (field_id, start, stop) {
-    for (let i = start; i < stop; i++) {
-        assert.ok(await $("#" + field_id + i).exists());
+    for (let i = start; i <= stop; i++) {
+        let enable = await $("#" + field_id + i).exists(0, 0);
+        assert.ok(enable);
     }
 });
-//LEDGER
 step("Displays <type> <word> from <start> to <stop>", async function (type, word, start, stop) {
     let decreaseNum = stop;
-    for (let i = start; i < stop; i++) {
+    for (let i = start; i <= stop; i++) {
         var content = await evaluate($("#" + type + i), (element) => element.innerText);
         assert.ok(content == word + decreaseNum);
         decreaseNum--;
     }
 });
-//LEDGER
 step("Verify <button_id> button is not visible", async function (button_id) {
     let visible = await button({id: button_id}).isVisible(0, 0);
     assert.ok(!visible);
 });
-//LEDGER
 step("Displays <type> <word>", async function (type, word) {
     var content = await evaluate($("#" + type), (element) => element.innerText);
     assert.ok(content == word);
 });
-/*
-step("Must display <message>", async function (message) {
-    assert.ok(await text(message).exists(0, 0));
+step("Displays <type> <word> from <start> to <stop> normal", async function (type, word, start, stop) {
+    for (let i = start; i <= stop; i++) {
+        var content = await evaluate($("#" + type + i), (element) => element.innerText);
+        assert.ok(content == word + i);
+    }
 });
-step("Add word <item> <placeholder>", async (item,placeholder) => {
-    await write(item, into(textBox(placeholder)));
+step("Verify <field_id> not exists", async function (field_id) {
+    let exist = await $("#" + field_id).exists();
+    console.log(exist);
+    assert.ok(!exist);
 });
-step("Must exist <type> <item>", async function (type,item) {
-    assert.ok(await type(item).exists(0, 0));
-});
-step("Click <item>", async (item) => {
-    await click(item);
-});
-step("Refresh page", async ()=> {
-    throw 'Unimplemented Step';
-});
-*/
